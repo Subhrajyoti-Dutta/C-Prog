f553ae91bc4360ab76789830f5affb8e8cf1b450
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2022-08-09 18:03:41 +0530

    Some changes

--- Practical 3/Q1.c
+++ Practical 3/Q1.c
@@ -24,7 +24,7 @@ int main() {
 	int count = 10;
 	printf("Enter the price of PC: ");
 	scanf("%lf", &price);
-	sum += price;
+	sum = price;
 	min = price;
 	max = price;
 	count--;
@@ -44,3 +44,19 @@ NEXT:
 	printf("The maximum price (in 1000s) is: %lf\n", max);
 	return 0;
 }
+
+/*
+Output: Enter the price of PC: 35.00
+		Enter the price of next PC: 40.50
+		Enter the price of next PC: 25.00
+		Enter the price of next PC: 31.25
+		Enter the price of next PC: 68.15
+		Enter the price of next PC: 47.00
+		Enter the price of next PC: 26.65
+		Enter the price of next PC: 29.00
+		Enter the price of next PC: 53.45
+		Enter the price of next PC: 62.50
+		The average price (in 1000s) is: 41.850000
+		The minimum price (in 1000s) is: 25.000000
+		The maximum price (in 1000s) is: 68.150000
+*/
