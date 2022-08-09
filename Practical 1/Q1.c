ec6dff4e4af3489454b3861ded95bbe61cc0d3d3
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2022-08-09 17:14:02 +0530

    Added output

--- Practical 1/Q1.c
+++ Practical 1/Q1.c
@@ -18,10 +18,10 @@ int main() {
 
 	//Printing the sum
 	int sum = num1 + num2 + num3;
-	printf("Sum of the values: %d\n", sum);
+	printf("Sum of the values is %d\n", sum);
 
 	//Printing the average
-	printf("Average of the values: %lf\n", sum / 3.0);
+	printf("Average of the values is %lf\n", sum / 3.0);
 
 	//Printing max
 	int max = num1, min = num1;
@@ -31,15 +31,23 @@ int main() {
 	if (num3 > max)
 		max = num2;
 
-	printf("Max of the values: %d\n", max);
+	printf("Max of the values is %d\n", max);
 	
 	if (num2 < min)
 		min = num2;
 	if (num3 < min)
 		min = num2;
 
-	printf("Min of the values: %d\n", min);
+	printf("Min of the values is %d\n", min);
 
 
 	return 0;
 }
+
+/*
+Output: Enter three numbers separated by space: 1 2 4
+        Sum of the values is 7
+        Average of the values is 2.333333
+        Max of the values is 4
+        Min of the values is 1
+*/
