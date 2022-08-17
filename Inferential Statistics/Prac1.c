5502b71a837ad34f643eaf14843a75946eaed3ad
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2022-08-17 13:43:35 +0530

    Some changes

--- Inferential Statistics/Prac1.c
+++ Inferential Statistics/Prac1.c
@@ -8,22 +8,33 @@ double genUnif(double a, double b) {
 	return a + (b - a) * z;
 }
 
+double myRound(double number, int prec) {
+	return round(number * pow(10, prec)) / pow(10, prec);
+}
+
 int main() {
 	srand(time(NULL));
+
 	double sum = 0, t;
 	double theta = 3;
 	int sampleSize = 100;
+
 	for (int i = 0; i < sampleSize; i++) {
-		t = genUnif(0, theta);
-		printf("%.2lf \n", t);
+		t = myRound(genUnif(0, theta), 2);
+		printf("%2d %5.2lf\n", i, t);
 		sum += t;
 	}
-	printf("%lf\n\n", 2 * sum / sampleSize);
-
+	double xbar = sum / sampleSize;
+	printf("\n  x̄  = %lf\n", xbar);
 
 	int CI = 95;
 	double var = pow(theta - 0, 2) / 12;
 	double MoE = 1.96 * sqrt(var / sampleSize);
+	double E_x = theta / 2;
+	printf("E[X] = %lf\n\n", E_x);
 
-	printf("At %d%% margin of error: %.2lf ± %.2lf\n", CI, theta, MoE);
+	printf("At %d%% margin of error: %.2lf\n", CI, MoE);
+	printf("|θ/2 - x̄| = %lf\n\n", fabs(E_x - xbar));
+
+	printf("Confidence interval: [ %lf , %lf ]\n", E_x - MoE, E_x + MoE);
 }
--- Inferential Statistics/Prac2.c
+++ Inferential Statistics/Prac2.c
@@ -8,21 +8,33 @@ double genExpo(double lambda) {
 	return -log(z) / lambda;
 }
 
+double myRound(double number, int prec) {
+	return round(number * pow(10, prec)) / pow(10, prec);
+}
+
 int main() {
 	srand(time(NULL));
+
 	double sum = 0, t;
 	double lambda = 0.2;
 	int sampleSize = 100;
+
 	for (int i = 0; i < sampleSize; i++) {
-		t = genExpo(lambda);
-		printf("%d %.2lf\n", i, t);
+		t = myRound(genExpo(lambda), 2);
+		printf("%2d %5.2lf\n", i, t);
 		sum += t;
 	}
-	printf("\nmu = %lf\n\n", sum / sampleSize);
+	double xbar = sum / sampleSize;
+	printf("\n  x̄  = %lf\n", xbar);
 
 	int CI = 95;
 	double var = 1 / (lambda * lambda);
 	double MoE = 1.96 * sqrt(var / sampleSize);
+	double E_x = 1 / lambda;
+	printf("E[X] = %lf\n\n", E_x);
 
-	printf("At %d%% margin of error: %.2lf ± %.2lf", CI, 1 / lambda, MoE);
+	printf("At %d%% margin of error: %.2lf\n", CI, MoE);
+	printf("|θ/2 - x̄| = %lf\n\n", fabs(E_x - xbar));
+
+	printf("Confidence interval: [ %lf , %lf ]\n", E_x - MoE, E_x + MoE);
 }
